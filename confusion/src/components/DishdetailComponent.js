eff17c539b7a65a4e06bb9214eac21b87a0ea98e
Sailesh-2209 <saileshr2209@gmail.com>
2020-10-31 01:07:27 +0530

    single page applications part 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 	function RenderComments({comments}) {
 		if (comments != null) {
@@ -28,6 +29,16 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 			return (
 				<div className="container">
 				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+						<BreadcrumbItem active>{props.selectedDish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.selectedDish.name}</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
 					<div className="col-12 col-md-5 m-1">
 						<Card>
 							<CardImg src={props.selectedDish.image} alt={props.selectedDish.name} />
@@ -38,7 +49,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 						</Card>
 					</div>
 					<div className="col-12 col-md-5 m-1">
-						<RenderComments comments={props.selectedDish.comments} />
+						<RenderComments comments={props.comments} />
 					</div>
 				</div>
 				</div>
