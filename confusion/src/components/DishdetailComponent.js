183c4527bc76c1be26cfc127d5720db80fac37ca
Sailesh-2209 <saileshr2209@gmail.com>
2020-11-02 20:19:28 +0530

    week 3 assignment

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
 
 	function RenderComments({comments}) {
 		if (comments != null) {
@@ -50,6 +51,7 @@ import { Link } from 'react-router-dom';
 					</div>
 					<div className="col-12 col-md-5 m-1">
 						<RenderComments comments={props.comments} />
+						<CommentForm />
 					</div>
 				</div>
 				</div>
